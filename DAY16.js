040518ad689957c0fa6e6fc2617612669c499112
saurabh2423 <mahajansaurabh2423@gmail.com>
2023-09-15 20:05:30 +0530

    added

---
+++ DAY16.js
@@ -0,0 +1,23 @@
+
+
+
+// let q = {
+//     firstname : saurabh,
+//     lastname : mahajan,
+//     age : 24,
+//     skills : javascript
+// }
+
+class person{
+    constructor(fn,ln,age,skills){
+        this.firstname = fn,
+        this.lastname = ln,
+        this.age = age,
+        this.skills = skills
+    }
+}
+
+
+let w = new person('shyam','kumar',26,'python')
+console.log(w)
+
